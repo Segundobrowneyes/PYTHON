ad66b8c1acbd52be2da36fc9a645da380dad61eb
Segundobrowneyes <segundocejas01@gmail.com>
2020-11-09 21:51:20 -0300

    Create floats.py

---
+++ CLASE_2/floats.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  9 21:42:45 2020
+Mini-desafío: floats
+Crear:
+
+Una función que convierta grados Celsius a grados Farenheit (https://es.wikipedia.org/wiki/Grado_Fahrenheit)
+Una función que convierta grados Celsius a grados Kelvin (https://es.wikipedia.org/wiki/Kelvin)
+El usuario debe ingresar una temperatura en grados Celsius y el programa debe mostrar las conversiones a Farenheit y Kelvin utilizando las funciones. Si la temperatura ingresada es inferior al cero absoluto, el programa debe mostrar un mensaje de advertencia.
+
+Pueden leer aqui sobre como hacer las conversiones.
+                    from Celsius	              to Celsius
+Fahrenheit	    [°F] = [°C] × ​9⁄5 + 32	       [°C] = ([°F] − 32) × ​5⁄9
+Kelvin	        [K] = [°C] + 273.15	           [°C] = [K] − 273.15
+
+
+
+            from Fahrenheit	            to Fahrenheit
+Celsius	   [°C] = ([°F] − 32) × ​5⁄9	  [°F] = [°C] × ​9⁄5 + 32
+Kelvin	   [K] = ([°F] + 459.67) × ​5⁄9	  [°F] = [K] × ​9⁄5 − 459.67
+
+
+
+
+@author: Madmax
+"""
+
